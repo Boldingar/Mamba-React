c98dab05c479c760512dbf82cbafa1e9fb4bc6bc
Mohammad Ahmad <m.a.7snen@gmail.com>
2025-04-21 21:50:02 +0200

    Added basic routing with no authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { ThemeProvider, CssBaseline } from "@mui/material";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { theme } from "./theme";
 import ChatPage from "./pages/ChatPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <ChatPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/chat" element={<ChatPage />} />
+          <Route path="/" element={<Navigate to="/chat" replace />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
