1060424dcef55bdd7191ead1d2fbc07876e55f2d
Islam Ahmed <islamahmedkader2004@gmail.com>
2025-04-23 03:05:38 +0200

    revert vite target

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const API_BASE_URL = import.meta.env.VITE_SERVER_URL;
-
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: API_BASE_URL,
+        target: "http://127.0.0.1:5000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
